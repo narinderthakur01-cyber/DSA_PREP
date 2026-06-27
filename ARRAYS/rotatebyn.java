1b4aa3a898980b2d38cf0d7f28faf053f5abfa92
narinderthakur01-cyber <narinderthakur2005@gmail.com>
2026-06-27 21:48:11 +0000

    Implement matrix rotation patterns

--- ARRAYS/rotatebyn.java
+++ ARRAYS/rotatebyn.java
@@ -8,7 +8,7 @@ class rotatebyn {
         };
 
         // 90° Clockwise Rotation
-        System.out.println("90° Clockwise");
+        System.out.println("90");
         for (int j = 0; j < arr[0].length; j++) {
             for (int i = arr.length - 1; i >= 0; i--) {
                 System.out.print(arr[i][j] + "\t");
@@ -17,26 +17,27 @@ class rotatebyn {
         }
 
         // 180° Rotation
-        System.out.println("\n180° Rotation");
-        for (int i = arr.length - 1; i >= 0; i--) {
-            for (int j = arr[0].length - 1; j >= 0; j--) {
-                System.out.print(arr[i][j] + "\t");
+        System.out.println("180");
+        for (int n = arr.length - 1; n >= 0; n--) {
+            for (int j = arr.length - 1; j >= 0; j--) {
+                System.out.print(arr[n][j] + "\t");
             }
             System.out.println();
         }
 
-        // Horizontal Mirror (Reverse each row)
-        // NOTE: This is NOT a 270° rotation.
-        System.out.println("\nHorizontal Mirror (Reverse Each Row)");
+        // Horizontal Mirror (Reverse Each Row)
+        // This was your original "270" code.
+        // It is a valid matrix transformation, but NOT a 270° rotation.
+        System.out.println("Horizontal Mirror");
         for (int a = 0; a < arr.length; a++) {
-            for (int j = arr[0].length - 1; j >= 0; j--) {
+            for (int j = arr.length - 1; j >= 0; j--) {
                 System.out.print(arr[a][j] + "\t");
             }
             System.out.println();
         }
 
         // Actual 270° Clockwise Rotation (90° Anti-Clockwise)
-        System.out.println("\n270° Clockwise / 90° Anti-Clockwise");
+        System.out.println("270");
         for (int j = arr[0].length - 1; j >= 0; j--) {
             for (int i = 0; i < arr.length; i++) {
                 System.out.print(arr[i][j] + "\t");
